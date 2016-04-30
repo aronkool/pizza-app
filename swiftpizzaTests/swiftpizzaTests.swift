f953cb987dd23dc1d64d22f0eeaf4aed1fc930bc
Aron Kool <aron.kool@topicus.nl>
2016-05-01 00:02:37 +0200

    [IOS] eerste opzet project

---
+++ swiftpizzaTests/swiftpizzaTests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftpizzaTests.swift
+//  swiftpizzaTests
+//
+//  Created by Aron Kool on 27-04-16.
+//  Copyright © 2016 Topicus. All rights reserved.
+//
+
+import XCTest
+@testable import swiftpizza
+
+class swiftpizzaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
